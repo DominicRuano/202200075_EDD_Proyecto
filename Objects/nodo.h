9d72964af7b375adbccecce3696c43d8f21f561a
DominicR <Dojuparupe1975@gmail.com>
2024-06-29 08:52:52 -0600

    Refactor Nodo class to encapsulate private members and provide public getters and setters

--- Objects/nodo.h
+++ Objects/nodo.h
@@ -2,7 +2,7 @@
 
 template <typename T>
 class Nodo{
-public:
+private:
     T data;
     int fila, columna;
     Nodo<T>* next;
@@ -10,5 +10,27 @@ public:
     Nodo<T>* down;
     Nodo<T>* up;
 
+public:
     Nodo(const T& value) : data(value), next(nullptr), prev(nullptr) {}
+
+    T getData() const {return data;}
+    void setData(const T& value) {data = value;}
+
+    int getFila() const {return fila;}
+    void setFila(int value) {fila = value;}
+
+    int getColumna() const {return columna;}
+    void setColumna(int value) {columna = value;}
+
+    Nodo<T>* getNext() const {return next;}
+    void setNext(Nodo<T>* node) {next = node;}
+
+    Nodo<T>* getPrev() const {return prev;}
+    void setPrev(Nodo<T>* node) {prev = node;}
+
+    Nodo<T>* getDown() const {return down;}
+    void setDown(Nodo<T>* node) {down = node;}
+
+    Nodo<T>* getUp() const {return up;}
+    void setUp(Nodo<T>* node) {up = node;}
 };
