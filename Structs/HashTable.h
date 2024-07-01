9e79a99a08a8ec260b77fcc714536c38ef49aaa2
DominicR <Dojuparupe1975@gmail.com>
2024-06-30 18:16:25 -0600

    Refactor HashTable.h to add graph() method

--- Structs/HashTable.h
+++ Structs/HashTable.h
@@ -1,4 +1,4 @@
-#include "./DoublyLinkedList.h"
+//#include "./DoublyLinkedList.h"
 #include <string>
 using namespace std;
 
@@ -16,13 +16,14 @@ public:
     int hashFunction(string key);
     void insert(T &val);
     T* search(string key);
+    void graph(ofstream &file, string ID, string str);
 };
 
 template <class T>
 HashTable<T>::HashTable(/* args */){
     size = 18;
     numElements = 0;
-    table = new DoublyLinkedList<T>[size]
+    table = new DoublyLinkedList<T>[size];
 }
 
 template <class T>
@@ -54,4 +55,24 @@ T* HashTable<T>::search(string key){
         current = current->next;
     }
     return nullptr;
+}
+
+template <class T>
+void HashTable<T>::graph(ofstream &file, string ID, string str){
+    int aux = 0;
+    file << "subgraph cluster_" << ID << " {" << endl;
+    file << "\tlabel=\"" << str << "\"; \n\tnode [shape=hexagon, style=filled, fillcolor=lightblue];" << endl;
+    file << "\tcolor=red;" << endl;
+    for (int i = 0; i < size; i++){
+        if (table[i].getHead() != nullptr){
+            file << "\tstruct" << ID << i << "[label=\"" << i << "\"];" << endl;
+            Nodo<T> *current = table[i].getHead();
+            while (current != nullptr){
+                file << "\tstruct" << ID << i << " -> " << current->data.getNumeroDeID() << endl;
+                current = current->next;
+                aux++;
+            }
+        }
+    }
+    file << "}" << endl;
 }
