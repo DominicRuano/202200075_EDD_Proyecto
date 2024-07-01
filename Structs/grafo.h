b628b00253e77bc79abeb0be8b0494596baebb69
DominicR <Dojuparupe1975@gmail.com>
2024-06-30 22:50:55 -0600

    FIX: grafo.h se arreglo el constructor, nuevoVertice, existeVertice, nuevoArco. se agrego el metodo para graficar.

--- Structs/grafo.h
+++ Structs/grafo.h
@@ -8,10 +8,9 @@ class Grafo{
 private:
     int numVertices;
     int maxVertices;
-    Nodo<T>* vertices;
+    Nodo<T>** vertices;
     int** matrizAdy;
 
-    ofstream archivo;
     string arco;
 public:
     Grafo(int max);
@@ -20,25 +19,28 @@ public:
     int getNumVertices(){return this->numVertices;}
     void setNumVertices(int n){this->numVertices = n;}
 
-    void nuevoVertice(string nombre);
+    void nuevoVertice(T nombre);
     int existeVertice(string nombre);
-    void nuevoArco(string nom1, string nom2);
+    void nuevoArco(string nom1, string nom2, int peso);
     void imprimirMatriz();
-    void generarReporte();
+    void generarReporte(ofstream &file, string ID, string str);
 };
 
 template <class T>
 Grafo<T>::Grafo(int max){
     numVertices = 0;
     maxVertices = max;
-    vertices = new Vertice[max];
     matrizAdy = new int*[max];
+    vertices = new Nodo<T>*[max]; // Inicializa el arreglo de punteros a nodos
+    for (int i = 0; i < max; ++i) {
+            vertices[i] = nullptr;
+    }
 
     for (int i = 0; i < max; i++)
         matrizAdy[i] = new int[max];
     for (int i = 0; i < maxVertices; i++)
         for (int j = 0; j < maxVertices; j++)
-            matrizAdy[i][j] = 0;
+            matrizAdy[i][j] = -1;
 }
 
 template <class T>
@@ -47,10 +49,10 @@ Grafo<T>::~Grafo(){
 }
 
 template <class T>
-void Grafo<T>::nuevoVertice(string nombre){
+void Grafo<T>::nuevoVertice(T nombre){
     bool existe = (existeVertice(nombre) >= 0);
     if (!existe){
-        Vertice nuevo = Vertice(nombre,numVertices);
+        Nodo<T>* nuevo = new Nodo<T>(nombre);
         vertices[numVertices] = nuevo;
         numVertices++;
     }
@@ -61,7 +63,7 @@ int Grafo<T>::existeVertice(string nombre){
     int i = 0;
     bool encontrado = false;
     while (i < numVertices && !encontrado){
-        encontrado = vertices[i].esIgual(nombre);
+        encontrado = vertices[i]->data == nombre;
         if (!encontrado)
             i++;
     }
@@ -69,12 +71,12 @@ int Grafo<T>::existeVertice(string nombre){
 }
 
 template <class T>
-void Grafo<T>::nuevoArco(string nom1, string nom2){
+void Grafo<T>::nuevoArco(string nom1, string nom2, int peso){
     int vertice1, vertice2;
     vertice1 = existeVertice(nom1);
     vertice2 = existeVertice(nom2);
     if (vertice1 >= 0 && vertice2 >= 0)
-        matrizAdy[vertice1][vertice2] = 1;  
+        matrizAdy[vertice1][vertice2] = peso;  
     else
         cout << "Error, uno de los vértices no existe\n";
 }
@@ -83,30 +85,29 @@ template <class T>
 void Grafo<T>::imprimirMatriz(){
     for (int i = 0; i < maxVertices; i++){
         for (int j = 0; j < maxVertices; j++)
-            cout << " " << matrizAdy[i][j] << " ";
+            cout << "[#]" << matrizAdy[i][j] << " ";
         cout << endl;
     }
 }
 
 template <class T>
-void Grafo<T>::generarReporte(){
+void Grafo<T>::generarReporte(ofstream &file, string ID, string str){
+    file << "subgraph cluster_" << ID << " {" << endl;
+    file << "\tlabel=\"" << str << "\"; \n\tnode [shape=hexagon, style=filled, fillcolor=lightblue];" << endl;
+    file << "\tcolor=green;" << endl;
     if (getNumVertices() == 0) {
-        cout << "Grafo vacío\n";
+        file << "\tstruct" << ID << "0 [label=\"Grafo vacío\", margin=0, shape=box, style=filled, color=none, fillcolor=none];\n}" << endl;
+        file << "agencia -> structF0[style=dotted];" << endl;
+        return;
     }else{
-        archivo.open("grafo.dot",ios::out);
-        archivo << "digraph G { " << endl;
-
-        for (int i = 0; i < maxVertices; i++)
-            for (int j = 0; j < maxVertices; j++)
-                if (matrizAdy[i][j] == 1){
-                    arco += vertices[i].getNombre() + " -> " + vertices[j].getNombre() + ";\n";
-                    archivo << arco;
+        for (int i = 0; i < numVertices; i++)
+            for (int j = 0; j < numVertices; j++)
+                if (matrizAdy[i][j] >= 0){
+                    arco += "\t" + vertices[i]->data + " -> " + vertices[j]->data + "[label = \"" + to_string(matrizAdy[i][j])  + "\", style=dashed];\n";
+                    file << arco;
                     arco.clear();
                 }
-
-        archivo << "}";
-        archivo.close();
-        system("dot -Tpng grafo.dot -o grafo.png");
-        system("start grafo.png");
     }
+    file << "}" << endl;
+    file << "agencia -> " << vertices[0]->data <<"[style=dotted];" << endl;
 }
