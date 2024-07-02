c0b67f8a9bfd22e9463c0b68fbfb347c4199c7e2
DominicR <Dojuparupe1975@gmail.com>
2024-07-01 18:54:07 -0600

    Refactor Avion.h to add copy() method for creating a copy of an avion object

--- Objects/Avion.h
+++ Objects/Avion.h
@@ -25,6 +25,7 @@ public:
     string getNumDeRegistro()const{return numDeRegistro;}
     string getEstado(){return estado;}
     string getDestino(){return ciudadDestino;}
+    avion* copy(){return new avion(vuelo, numDeRegistro, modelo, capacidad, ciudadDestino, estado);}
 };
 
 avion::avion(string vuelo, string numDeRegistro, string modelo, int capacidad, string ciudadDestino, string estado){
